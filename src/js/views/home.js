85987210652b295d3f8988ff16f498ae2b4a359a
chrisperez22 <chrisperez0531@gmail.com>
2022-12-14 00:34:31 +0000

    sec

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,12 +1,16 @@
-
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import sone from "../../img/greyjob.jpg"
-import stwo from "../../img/favorite2.jpg"
-import sthree from "../../img/favorite.jpg"
-import backtwo from "../../img/back2.jpg"
-import sfive from "../../img/texture.jpg"
-import check from "../../img/checkmark.jpg"
+import sone from "../../img/greyjob.jpg";
+import stwo from "../../img/favorite2.jpg";
+import sthree from "../../img/favorite.jpg";
+import backtwo from "../../img/back2.jpg";
+import sfive from "../../img/texture.jpg";
+import check from "../../img/checkmark.jpg";
+import pay from "../../img/payment.jpg";
+import pool from "../../img/pooldeck.jpg";
+import work2 from "../../img/fence.jpg";
+import work3 from "../../img/euro.jpg";
+import work4 from "../../img/tile.jpg";
 
 import { Context } from "../store/appContext";
 
@@ -14,113 +18,181 @@ import "../../styles/home.css";
 
 export const Home = () => (
   <div className="every bg-light">
-	<div className="slide">
-		<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img id="slideshow"src={sthree} class="d-block w-100" alt="..."/>
+    <div className="slide">
+      <div
+        id="carouselExampleFade"
+        class="carousel slide carousel-fade"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img id="slideshow" src={sthree} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img id="slideshow" src={sone} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img id="slideshow" src={backtwo} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img id="slideshow" src={stwo} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img id="slideshow" src={sfive} class="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
-    <div class="carousel-item">
-      <img id="slideshow"src={sone} class="d-block w-100" alt="..."/>
+
+    <br></br>
+    <div class="ser">
+      <div class="services">
+        <h3>𝐒𝐞𝐫𝐯𝐢𝐜𝐞𝐬</h3>
+      </div>
+
+      <div class="row">
+        <div class="col-4">
+          <h5>𝙎𝙩𝙖𝙢𝙥𝙚𝙙 𝘾𝙤𝙣𝙘𝙧𝙚𝙩𝙚 </h5>
+
+          <img class="right" src={work4}></img>
+          <p1 id="service">
+            Stamped concrete is frequently used for pool decks, patios, and
+            driveways. The appealing and long-lasting alternative to
+            conventional concrete is stamped concrete. Because it can produce a
+            distinctive and fashionable look that is also long-lasting and
+            simple to maintain.
+          </p1>
+        </div>
+
+
+
+
+
+        <div class="col-4">
+    
+        <img class="right" src={pool}></img>
+          <h5>𝘾𝙤𝙣𝙘𝙧𝙚𝙩𝙚 𝙋𝙤𝙤𝙡 𝘿𝙚𝙘𝙠</h5>
+          <p1 id="service">
+            For a pool deck, concrete is a fantastic material because it is
+            sturdy and long-lasting.Your pool deck can also be made
+            more interesting and beautiful by stamping concrete. A concrete
+            contractor will be able to install your deck effectively and assist
+            you in selecting the best material.
+          </p1>
+         
+        </div>
+
+
+
+
+
+
+
+
+
+        <div class="col-4">
+        <img class="right" src={work2}></img>
+          <h5>𝘼𝙡𝙪𝙢𝙞𝙣𝙪𝙢 𝙁𝙚𝙣𝙘𝙞𝙣𝙜 𝙄𝙣𝙨𝙩𝙖𝙡𝙡𝙖𝙩𝙞𝙤𝙣 </h5>
+          <p1 id="service">
+            They are durable, easy to install, maintenance free and an
+            affordable fencing solution for every property. Above all, when
+            installed correctly they look great and enhance the style of your
+            property.
+          </p1>
+        </div>
+
+        <div class="col-4">
+          <h5>𝙋𝙡𝙖𝙞𝙣 𝘾𝙤𝙣𝙘𝙧𝙚𝙩𝙚 </h5>
+          <p1 id="service">
+            Plain concrete is most commonly used for paving and flooring. Made
+            from a mix of cement, aggregate, and water the different types of
+            aggregate and ratio of materials used will give different types of
+            cement with slightly different properties.
+          </p1>
+        </div>
+        <div class="col-4">
+        <img class="right" src={work3}></img>
+          <h5>𝙀𝙪𝙧𝙤 𝙏𝙞𝙡𝙚</h5>
+          <p1 id="service">
+            Eurotile is a product that consists of acrylic and concrete. It is
+            applied over concrete and most any other sound and solid surface.
+          </p1>
+        </div>
+        <div class="col-4">
+          <h5>𝙎𝙞𝙙𝙚𝙬𝙖𝙡𝙠</h5>
+          <p1 id="service"></p1>
+        </div>
+      </div>
     </div>
-    <div class="carousel-item">
-      <img id="slideshow"src={backtwo} class="d-block w-100" alt="..."/>
+
+    <form class="contact row g-3 bg-light">
+      <h3>Get in Contact with us for a FREE estimate !</h3>
+      <div class="col-md-4">
+        <label for="inputName4" class="form-label"></label>
+        <input
+          type="name"
+          class="form-control"
+          id="inputName4"
+          placeholder="Name:"
+        />
+      </div>
+      <div class="col-md-4">
+        <label for="inputPhone4" class="form-label"></label>
+        <input
+          type="phone"
+          class="form-control"
+          id="inputPhone4"
+          placeholder="Phone:"
+        />
+      </div>
+      <div class="col-md-4">
+        <label for="inputEmail" class="form-label"></label>
+        <input
+          type="email"
+          class="form-control"
+          id="inputEmail"
+          placeholder="Email:"
+        />
+      </div>
+      <div class="message col-12">
+        <label for="inputMessage2" class="form-label"></label>
+        <input
+          type="text"
+          class="form-control"
+          id="inputMessage2"
+          placeholder="Message:"
+        />
+      </div>
+      <br></br> <br></br> <br></br> <br></br> <br></br>
+      <div class="col-12">
+        <button class="btn btn-primary" type="submit">
+          Send
+        </button>
+      </div>
+    </form>
+    <br></br>
+    <br></br>
+    <br></br>
+    <div className="card bg-light">
+      <img src={pay} />
     </div>
-    <div class="carousel-item">
-      <img id="slideshow"src={stwo} class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img id="slideshow"src={sfive} class="d-block w-100" alt="..."/>
-    </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-	</div>
-
-{/* // under slideshow// */}
-<br></br>
-<div class="ser">
-<div class="services">
-<h3>𝐒𝐞𝐫𝐯𝐢𝐜𝐞𝐬</h3>
-</div> 
-
-
-<div class="row">
-<div class="col-4">
-
-<h5>>𝙎𝙩𝙖𝙢𝙥𝙚𝙙 𝘾𝙤𝙣𝙘𝙧𝙚𝙩𝙚 </h5>
-
-{/* <img class="mark"src={check}></img */}
-  <p1 id="service">Stamped concrete is frequently used for pool decks, patios, and driveways. The appealing and long-lasting alternative to conventional concrete is stamped concrete. Because it can produce a distinctive and fashionable look that is also long-lasting and simple to maintain.</p1>
-</div>
-<div class="col-4">
-<h5>>𝘾𝙤𝙣𝙘𝙧𝙚𝙩𝙚 𝙋𝙤𝙤𝙡 𝘿𝙚𝙘𝙠</h5>
-  <p1 id="service">
-  For a pool deck, concrete is a fantastic material because it is sturdy and long-lasting. With the right care and maintenance, concrete can last for many years. Your pool deck can also be made more interesting and beautiful by stamping concrete.  A concrete contractor will be able to install your deck effectively and assist you in selecting the best material.
-</p1>
-</div>
-<div class="col-4">
-
-<h5>>𝘼𝙡𝙪𝙢𝙞𝙣𝙪𝙢 𝙁𝙚𝙣𝙘𝙞𝙣𝙜 𝙄𝙣𝙨𝙩𝙖𝙡𝙡𝙖𝙩𝙞𝙤𝙣  </h5>
-  <p1 id="service">They are durable, easy to install, maintenance free and an affordable fencing solution for every property. Above all, when installed correctly they look great and enhance the style of your property.</p1>
-</div>
-
-<div class="col-4">
-<h5>>𝙋𝙡𝙖𝙞𝙣 𝘾𝙤𝙣𝙘𝙧𝙚𝙩𝙚 </h5>
-  <p1 id="service">Plain concrete is most commonly used for paving and flooring. Made from a mix of cement, aggregate, and water the different types of aggregate and ratio of materials used will give different types of cement with slightly different properties.</p1>
-</div>
-<div class="col-4">
-<h5>>𝙀𝙪𝙧𝙤 𝙏𝙞𝙡𝙚</h5>
-  <p1 id="service">Eurotile is a product that consists of acrylic and concrete. It is applied over concrete and most any other sound and solid surface.</p1>
-</div>
-<div class="col-4">
-<h5>>𝙎𝙞𝙙𝙚𝙬𝙖𝙡𝙠</h5>
-  <p1 id="service"></p1>
-</div>
-</div>
-
-</div>
-
-
-
-
-
-<form class="contact row g-3 bg-light">
-  <h1>Get in Contact with us for a FREE estimate !</h1>
-  <div class="col-md-4">
-    <label for="inputName4" class="form-label"></label>
-    <input type="name" class="form-control" id="inputName4" placeholder="Name:"/>
-  </div>
-  <div class="col-md-4">
-    <label for="inputPhone4" class="form-label"></label>
-    <input type="phone" class="form-control" id="inputPhone4" placeholder="Phone:"/>
-  </div>
-  <div class="col-md-4">
-    <label for="inputEmail" class="form-label"></label>
-    <input type="email" class="form-control" id="inputEmail" placeholder="Email:"/>
-  </div>
-  
-  <div class="message col-12">
-    <label for="inputMessage2" class="form-label"></label>
-    <input type="text" class="form-control" id="inputMessage2" placeholder="Message:"/>
-  </div>
-  <br></br> <br></br> <br></br> <br></br> <br></br>
-  <div class="col-12">
-    <button class="btn btn-primary" type="submit">Send</button>
-  </div>
-  
-  </form>
-  <br></br><br></br><br></br><br></br><br></br><br></br>
-
-
-</div>
-
-	
 );
--- src/js/views/services.js
+++ src/js/views/services.js
@@ -2,19 +2,110 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { Context } from "../store/appContext";
-
+import work4 from "../../img/tile.jpg";
+import pool from "../../img/pooldeck.jpg";
+import work2 from "../../img/fence.jpg";
+import work3 from "../../img/euro.jpg";
+import work5 from "../../img/job9.jpg";
 import "../../styles/services.css";
 
 export const Services = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container">
-			<p>hello</p>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
+		<div class="container text-center">
+  <div class="row justify-align">
+    <div class="first col">
+		<div class="head bg-black">
+      <h1>𝕊𝕥𝕒𝕞𝕡𝕖𝕕 ℂ𝕠𝕟𝕔𝕣𝕖𝕥𝕖</h1>
+	  </div>
+	  <div>
+		<img class="pic1" src={work4}></img>
+		<div class="footer1">
+		The appealing and long-lasting alternative to
+            conventional concrete is stamped concrete. Because it can produce a
+            distinctive and fashionable look that is also long-lasting and
+            simple to maintain.
 		</div>
+	  </div>
+	  
+    </div>
+
+    <div class="first col">
+	<div class="head bg-black">
+      <h1>ℂ𝕠𝕟𝕔𝕣𝕖𝕥𝕖 ℙ𝕠𝕠𝕝 𝔻𝕖𝕔𝕜</h1>
+	  </div>
+	  <div>
+	  <img class="pic2" src={pool}></img>
+	  <div class="footer">
+		For a pool deck, concrete is a fantastic material because it is
+            sturdy and long-lasting.Your pool deck can also be made
+            more interesting and beautiful by stamping concrete. 
+			
+		</div>
+	  </div>
+    </div>
+    <div class="first col">
+	<div class="head bg-black">
+      <h1>𝔼𝕦𝕣𝕠 𝕋𝕚𝕝𝕖</h1>
+	  </div>
+	  <div>
+		<img class="pic1" src={work3}></img>
+		<div class="footer3">
+		Eurotile is a product that consists of acrylic and concrete. It is applied over concrete and most any other sound and solid surface.
+
+		</div>
+	  </div>
+    </div>
+  </div>
+  <br></br>
+  <div class="row">
+    <div class="first col">
+		<div class="head bg-black">
+      <h1>𝔸𝕝𝕦𝕞𝕚𝕟𝕦𝕞 𝔽𝕖𝕟𝕔𝕚𝕟𝕘 𝕀𝕟𝕤𝕥𝕒𝕝𝕝𝕒𝕥𝕚𝕠𝕟</h1>
+	  </div>
+	  <div>
+		<img class="pic1" src={work2}></img>
+		<div class="foote">
+		They are durable, easy to install, maintenance free and an
+            affordable fencing solution for every property. Above all, when
+            installed correctly they look great and enhance the style of your
+            property.
+		</div>
+	  </div>
+	  
+    </div>
+
+    <div class="first col">
+	<div class="head bg-black">
+      <h1>ℙ𝕝𝕒𝕚𝕟 ℂ𝕠𝕟𝕔𝕣𝕖𝕥𝕖</h1>
+	  </div>
+	  <div>
+		<img class="pic1" src={work4}></img>
+		<div class="footer2">
+		Plain concrete is most commonly used for paving and flooring. Made
+            from a mix of cement, aggregate, and water the different types of
+            aggregate and ratio of materials used will give different types of
+            cement with slightly different properties.
+		</div>
+	  </div>
+    </div>
+    <div class="first col">
+	<div class="head bg-black">
+      <h1>𝔸𝕣𝕥𝕚𝕗𝕚𝕔𝕚𝕒𝕝 𝔾𝕣𝕒𝕤𝕤</h1>
+	  </div>
+	  <div>
+		<img class="pic1" src={work5}></img>
+		<div class="footer4">
+		Stamped concrete is frequently used for pool decks, patios, and
+            driveways. The appealing and long-lasting alternative to
+            conventional concrete is stamped concrete. Because it can produce a
+            distinctive and fashionable look that is also long-lasting and
+            simple to maintain.
+		</div>
+	  </div>
+    </div>
+  </div>
+</div>
 	);
 };
